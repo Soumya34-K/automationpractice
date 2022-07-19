e934040fff42f43e1b7accb3d6e9a1f81402ae32
Soumya34-K <ksoumya34@gmail.com>
2022-07-19 15:30:57 +0530

    New commit

---
+++ test/WebElementInteractions.test.js
@@ -0,0 +1,43 @@
+const webPage=require ('../pages/element.pages');
+
+describe("Element actions on the page",  async () => {
+
+    it("Get the title of the page",  async () => {
+
+        await browser.url('/;')
+        webPage.pageTitle
+    
+    })
+
+
+    it('should search for a product and verify the search text value',  async () =>{
+    
+     webPage.searchInput.addValue('T-SHIRTS ');
+     webPage.searchBtn.click;
+     await browser.pause(3000);
+
+    expect(webPage.searchInput).toHaveValue('T-SHIRTS')
+
+    })
+    it('should disply My order footer link ', async () => {
+       let ele =  webPage.footerLink
+       console.log(ele.isDisplayed);
+        
+    });
+    it('Should click on the link', async() => {
+         webPage.clickLink.click
+         await browser.pause(3000);
+      
+
+        
+    });
+
+
+ 
+})
+
+
+
+
+
+
