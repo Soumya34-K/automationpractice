99a207534e987a2c1ec3383ccafde2e5a9baccd1
Soumya34-K <ksoumya34@gmail.com>
2022-07-22 12:43:04 +0530

    new commit

--- test/WebElementInteractions.test.js
+++ test/WebElementInteractions.test.js
@@ -1,36 +1,61 @@
-const { searchInput } = require('../pages/element.pages');
+
 const webPage=require ('../pages/element.pages');
 
 describe("Element actions on the page",  async () => {
 
     it("Get the title of the page",  async () => {
 
-        await browser.url('/')
+        await browser.url('/');
         webPage.pageTitle
     
     });
 
 
     it('should search for a product and verify the search text value',  async () =>{
+     
     
-     webPage.searchInput.addValue('T-SHIRTS ');
-     webPage.searchBtn.click;
+     webPage.addElementValue
      await browser.pause(3000);
 
-    expect(webPage.searchInput).toHaveValue('T-SHIRTS')
+     expect(webPage.searchInput).toHaveValue('T-SHIRTS')
 
     });
 
     it('should disply My order footer link ', async () => {
-       let ele =  webPage.footerLink
-       console.log(ele.isDisplayed);
+
+        
+      console.log(webPage.getFooterLink);
         
     });
 
     it('Should click on the link', async() => {
-        webPage.clickLink.click
-        await browser.pause(3000);       
+    
+        webPage.clickLink
+        await browser.pause(2000);       
     });
+
+    it('Should click on add to cart button', async() => {
+
+        await browser.url('/');
+        webPage.addElementToCart
+        await browser.pause(2000); 
+    });
+
+    it('Should mouse hover on Women and click it ', async() => {
+
+        await browser.url('/')
+        webPage.clickLinkWomen
+        await browser.pause(2000); 
+    });
+
+    it('Should click on Tops ', async() => {
+
+       
+        webPage.clickLinkTops
+        await browser.pause(2000); 
+        
+    });
+
  
 });
 
