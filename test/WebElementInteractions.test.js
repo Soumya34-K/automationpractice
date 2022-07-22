11bbb509e5c6e8869e37875a7efc757260f0841e
Soumya34-K <ksoumya34@gmail.com>
2022-07-22 19:02:23 +0530

    Added test cases

--- test/WebElementInteractions.test.js
+++ test/WebElementInteractions.test.js
@@ -50,7 +50,7 @@ describe("Element actions on the page",  async () => {
 
     it('Should click on Tops ', async() => {
 
-       
+        
         webPage.clickLinkTops
         await browser.pause(2000); 
         
@@ -58,7 +58,9 @@ describe("Element actions on the page",  async () => {
 
     it('Should count total products', async() => {
         
-       webPage.getCount
+       
+        webPage.getCount
+        await browser.pause(2000); 
     });
 
  
