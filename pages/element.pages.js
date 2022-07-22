75d2689578593ccc31ef5c6e169a53a33c61a238
Soumya34-K <ksoumya34@gmail.com>
2022-07-22 12:40:28 +0530

    added scripts

--- pages/element.pages.js
+++ pages/element.pages.js
@@ -1,6 +1,5 @@
 const { default: $ } = require("webdriverio/build/commands/browser/$");
 
-
 class AutomationPracticePage {
 
     get pageTitle() {
@@ -23,14 +22,53 @@ class AutomationPracticePage {
 
         return $ ("//a[@title='My orders']")
     }
-    get clickLink () {
+    get link() {
 
         return $ ("//a[@title='Contact Us']")
     }
+    get addToCart() {
+ 
+        return $ ("//ul[@id='homefeatured']//li[@class='ajax_block_product col-xs-12 col-sm-4 col-md-3 last-item-of-mobile-line']//span[contains(text(),'Add to cart')]");
+    }
+    get mouseHover() {
+
+        return $ ("//a[@title='Women']")
+    }
+    get clickTops () {
+
+        return $("//a[@title='Tops']")
 
 
+    }
+    getFooterLink (){
+
+        this.footerLink.isDisplayed()
+    }
+    addElementValue (){
+
+        this.searchInput.setValue('T-SHIRTS ');
+        this.searchBtn.click()
 
 
+    }
+    clickLink () {
+
+        this.link.click()
+    }
+    addElementToCart() {
+
+        this.addElementToCart.click()
+     
+    }
+    clickLinkWomen () {
+        this.mouseHover.click()
+    }
+
+    clickLinkTops () {
+
+        this.clickLinkTops.click ()
+    }
+    
  
 }
 
